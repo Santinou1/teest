d4a35c2862db8f1492f6883afaf4ab9612679769
Santino <101665218+Santinou1@users.noreply.github.com>
2022-12-07 19:01:52 -0300

    Add files via upload

---
+++ components/LayOut.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Footer from './Footer'
+import Navbar from './Navbar'
+
+
+
+export default function LayOut() {
+  return (
+    <>
+    <div>
+      <Navbar/>
+      <div>
+        
+      </div>
+      <Footer/>
+    </div>
+    </>
+  )
+}
---
+++ pages/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LayOut from "../components/LayOut"
+
+export default function Home() {
+  return (
+    <div>
+      {<LayOut/>}
+    </div>
+  )
+}
